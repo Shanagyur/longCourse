c42bdd4c50043673555193679f25e2b7c85b3dfc
PT <PT@PT-PC>
2017-04-28 13:47:39 +0200

    - add api, console, persist, service modul

---
+++ bundle/console/src/main/java/hu/me/iit/carshop/console/App.java
@@ -0,0 +1,13 @@
+package hu.me.iit.carshop.console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
